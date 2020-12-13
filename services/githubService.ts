d63741dab0ff32b29be104a3ca7fb67361d7ce3a
Yorrick Bakker <y.bakker@me.com>
2020-12-13 01:13:42 +0100

    Use readme as fallback

--- services/githubService.ts
+++ services/githubService.ts
@@ -1,21 +1,48 @@
 import env from '../env.ts'
 import * as log from 'https://deno.land/std@0.76.0/log/mod.ts'
-import { GithubURL, GithubFileResponse } from '../types/common.ts'
+import { GithubURL, GithubFileResponse, GithubFile } from '../types/common.ts'
 
 const TOKEN = env('GITHUB_TOKEN')
 const BASE_URL = 'https://api.github.com'
 
+const parseGithubUrl = (url: string): GithubURL => {
+  const githubURL = new URL(url)
+  if (githubURL.pathname === '/') throw 'invalid path'
+  const path = githubURL.pathname.split('/').filter(p => p !== '')
+  const sections = path.filter(s => s !== 'blob')
+
+  return {
+    user: sections[0],
+    repository: sections[1],
+    ref: sections[2],
+    path: sections.slice(3).join('/'),
+    file: sections.slice(-1)[0],
+    extension: sections.slice(-1)[0].split('.').slice(-1)[0],
+  }
+}
+
 const getGithubFile = async (
   fileUrl: GithubURL
 ): Promise<GithubFileResponse> => {
-  const url = `${BASE_URL}/repos/${fileUrl.user}/${fileUrl.repository}/contents/${fileUrl.path}`
+  const url = `${BASE_URL}/repos/${fileUrl.user}/${fileUrl.repository}/contents/${fileUrl.path}?ref=${fileUrl.ref}`
   const fileRequest = await fetch(url, getFetchOptions(TOKEN!))
 
-  if (!fileRequest.ok) return { response: fileRequest }
+  let file = await fileRequest.json()
+
+  if (file instanceof Array) {
+    log.info(file.length)
+    const s: GithubFile = file.find(
+      (f: GithubFile) => f.name.toLowerCase() === 'readme.md'
+    )
+    if (s === undefined) throw 'File not found'
+    const readme = `${BASE_URL}/repos/${fileUrl.user}/${fileUrl.repository}/contents/${fileUrl.path}/${s.name}?ref=${fileUrl.ref}`
+    file = await fetch(readme, getFetchOptions(TOKEN!)).then(res => res.json())
+  }
+  log.info(file)
 
   return {
     response: fileRequest,
-    file: { ...(await fileRequest.json()), url: fileUrl },
+    file: { ...file, url: fileUrl },
   }
 }
 
@@ -34,4 +61,4 @@ const getFetchHeaders = (token: string): Headers => {
   return headers
 }
 
-export { getGithubFile }
+export { getGithubFile, parseGithubUrl }
