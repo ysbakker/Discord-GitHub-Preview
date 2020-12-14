ec12b0ce94223da67e11ac67d6c1d32d8a2d3cf9
Yorrick Bakker <y.bakker@me.com>
2020-12-14 01:24:09 +0100

    Fall back on repository readme instead of folder
    Also refactoring

--- services/githubService.ts
+++ services/githubService.ts
@@ -1,6 +1,6 @@
 import env from '../env.ts'
 import * as log from 'https://deno.land/std@0.76.0/log/mod.ts'
-import { GithubURL, GithubFileResponse, GithubFile } from '../types/common.ts'
+import { GithubURL, GithubFileResponse, GithubFile } from '../types/github.ts'
 
 const TOKEN = env('GITHUB_TOKEN')
 const BASE_URL = 'https://api.github.com'
@@ -16,37 +16,38 @@ const parseGithubUrl = (url: string): GithubURL => {
     repository: sections[1],
     ref: sections[2],
     path: sections.slice(3).join('/'),
-    file: sections.slice(-1)[0],
-    extension: sections.slice(-1)[0].split('.').slice(-1)[0],
   }
 }
 
 const getGithubFile = async (
   fileUrl: GithubURL
 ): Promise<GithubFileResponse> => {
+  let file: GithubFile
   const url = `${BASE_URL}/repos/${fileUrl.user}/${fileUrl.repository}/contents/${fileUrl.path}?ref=${fileUrl.ref}`
+
   const fileRequest = await fetch(url, getFetchOptions(TOKEN!))
+  const fileRequestResult = await fileRequest.json()
+  let readmeRequest: Response | undefined
 
-  let file = await fileRequest.json()
+  if (!fileRequest.ok || fileRequestResult instanceof Array) {
+    const readmeUrl = `${BASE_URL}/repos/${fileUrl.user}/${fileUrl.repository}/readme?ref=${fileUrl.ref}`
 
-  if (file instanceof Array) {
-    const s: GithubFile = file.find(
-      (f: GithubFile) => f.name.toLowerCase() === 'readme.md'
-    )
-    if (s === undefined) throw 'File not found'
-    const readme = `${BASE_URL}/repos/${fileUrl.user}/${fileUrl.repository}/contents/${fileUrl.path}/${s.name}?ref=${fileUrl.ref}`
-    fileUrl = {
-      ...fileUrl,
-      extension: 'md',
-      file: s.name,
-      path: `${fileUrl.path}/${s.name}`,
-    }
-    file = await fetch(readme, getFetchOptions(TOKEN!)).then(res => res.json())
-  }
+    readmeRequest = await fetch(readmeUrl, getFetchOptions(TOKEN!))
+    if (!readmeRequest.ok)
+      return { response: !fileRequest.ok ? fileRequest : readmeRequest }
+
+    const readme: GithubFile = await readmeRequest.json()
+    fileUrl = { ...fileUrl, path: readme.path }
+    file = readme
+  } else file = fileRequestResult
 
   return {
-    response: fileRequest,
-    file: { ...file, url: fileUrl },
+    response: readmeRequest || fileRequest,
+    file: {
+      ...file,
+      extension: file.name.split('.').slice(-1)[0],
+      url: fileUrl,
+    },
   }
 }
 
